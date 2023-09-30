6b5a3c9192ecce8cffc2b2022576b8fe2fa87615
wxh <2083793542@qq.com>
2023-09-30 21:33:59 +0800

    练习变量1

---
+++ sources/练习01.py
@@ -0,0 +1,10 @@
+money = 50
+print("当前钱包余额：", money)
+# 购买冰淇凌花了十元
+print("购买冰淇凌花费：", 10, '元')
+money = money - 10
+# 购买可乐花费五元
+print("购买可乐花费：", 5, '元')
+money = money - 5
+print("最终，钱包剩余：", money, '元')
+
