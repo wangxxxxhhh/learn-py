333db5f246a0ef02daf314ac300d04fbdf1e2548
wxh <2083793542@qq.com>
2023-10-13 12:02:43 +0800

    字符串格式化

---
+++ sources/判断语句综合案例.py
@@ -0,0 +1,23 @@
+import random
+num = random.randint(1, 10)
+guess_num = int(input("输入你要猜测的数字:"))
+if guess_num == num:
+    print("恭喜第一次就猜对了。")
+else:
+    if guess_num > num:
+        print("猜大了。")
+    else:
+        print("猜小了。")
+    guess_num = int(input("输入第二次猜测的数字:"))
+    if guess_num == num:
+        print("恭喜第二次猜对了。")
+    else:
+        if guess_num > num:
+            print("猜大了。")
+        else:
+            print("猜小了。")
+    guess_num = int(input("输入第三次猜测的数字:"))
+    if guess_num == num:
+        print("恭喜第三次猜对了。")
+    else:
+        print("三次都猜错了。")
