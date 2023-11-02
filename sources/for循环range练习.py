14debe76c20761a555d348c9d2bc265e57a7058a
wxh <2083793542@qq.com>
2023-11-02 09:24:10 +0800

    for循环基础

---
+++ sources/for循环range练习.py
@@ -0,0 +1,5 @@
+count = 0
+for x in range(1, 100):
+    if x % 2 == 0:
+        count += 1
+print(f"一共出现了{count}次偶数")
---
+++ sources/for循环练习.py
@@ -0,0 +1,8 @@
+name = "iteima is a brand of itcast"
+count = 0
+for x in name:
+    if x == "a":
+        count += 1
+print(f"被统计数字中有{count}个a")
+
+
