b7de2988d8b335cdb0b6fecc0172abd7c1966a1b
wxh <2083793542@qq.com>
2023-12-01 15:48:06 +0800

    集合

---
+++ sources/集合.py
@@ -0,0 +1,5 @@
+my_list = ['黑马程序员', '传智播客', '黑马程序员', '传智播客', 'itheima', 'itcast', 'itcast', 'best']
+set1 = set()
+for element in my_list:
+    set1.add(element)
+print(f"列表中的内容是:{my_list}\n, 遍历到集合中的元素是{set1}")
