1ad808d9278b8074cc12291fef485db9744eb5de
wxh <2083793542@qq.com>
2024-01-31 11:07:17 +0800

    继承

---
+++ sources/继承.py
@@ -0,0 +1,16 @@
+class phone:
+    producer = "小米"
+    def call_by_4g(self):
+        print("正在使用4g网络")
+    def id(self):
+        print("id编码是666")
+
+class new_phone(phone):
+    def call_by_5g(self):
+        print(f"厂商是{super().producer}")
+        phone.call_by_4g(self)
+        super().id()
+        print("正在使用5g通话")
+new_phone = new_phone()
+new_phone.call_by_5g()
+
