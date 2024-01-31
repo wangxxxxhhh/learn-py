a1cd40ec10aa4214e7351d7831d5940d34647f19
wxh <2083793542@qq.com>
2024-01-31 11:07:56 +0800

    继承

--- sources/继承.py
+++ sources/继承.py
@@ -1,9 +1,12 @@
 class phone:
     producer = "小米"
+
     def call_by_4g(self):
         print("正在使用4g网络")
+
     def id(self):
         print("id编码是666")
+
 
 class new_phone(phone):
     def call_by_5g(self):
@@ -11,6 +14,7 @@ class new_phone(phone):
         phone.call_by_4g(self)
         super().id()
         print("正在使用5g通话")
-new_phone = new_phone()
-new_phone.call_by_5g()
 
+
+New_phone = new_phone()
+new_phone.call_by_5g()
